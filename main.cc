87cee830e42cc77df491c73b9152ad5237901895
Joan Bonet Orantos <orantos.gmail.com>
2018-04-08 14:19:00 +0200

    Up to chapter 8. Lambertian and metal material

--- main.cc
+++ main.cc
@@ -3,14 +3,25 @@
 #include "hitable_list.h"
 #include "float.h"
 #include "camera.h"
+#include "material.h"
 
-vec3 color(const ray &r, hitable *world) 
+vec3 color(const ray &r, hitable *world, int depth) 
 {
     hit_record rec;
 
-    if(world->hit(r, 0.0, FLT_MAX, rec))
+    if(world->hit(r, 0.001, FLT_MAX, rec))
     {
-        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+        ray scattered;
+        vec3 attenuation;
+
+        if(depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+        {
+            return attenuation * color(scattered, world, depth + 1);
+        }
+        else
+        {
+            return vec3(0, 0, 0);
+        }
     }
     else
     {
@@ -29,13 +40,15 @@ int main()
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    hitable *list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hitable *list[4];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100,  new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 1.0));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.3));
 
     camera cam;
 
-    hitable *world = new hitable_list(list, 2);
+    hitable *world = new hitable_list(list, 4);
 
     for (int j = ny - 1; j >= 0; j--)
     {
@@ -49,10 +62,11 @@ int main()
                 float v = float(j + drand48()) / float(ny);
 
                 ray r = cam.get_ray(u, v);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
 
             col /= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);

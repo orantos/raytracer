655aa66f0762d3feac468890144a4f230f2a5ca4
Joan Bonet Orantos <orantos.gmail.com>
2018-04-12 21:42:53 +0200

    Up to chapter 11. Depth of field

--- camera.hpp
+++ camera.hpp
@@ -3,13 +3,25 @@
 
 #include "ray.hpp"
 
+vec3 random_in_unit_disk()
+{
+	vec3 p;
+	do
+	{
+		p = 2.0 * vec3(drand48(), drand48(), 0) - vec3(1, 1 ,0);
+	}
+	while(dot(p, p) >= 1.0);
+
+	return p;
+}
+
 class camera
 {
 	public:
 		// vfov is top to bottom in degrees
-		camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect)
+		camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist)
 		{
-			vec3 u, v, w;
+			lens_radius = aperture / 2;
 			float theta = vfov * M_PI / 180;
 			float half_height = tan(theta / 2);
 			float half_width = aspect * half_height;
@@ -20,20 +32,25 @@ class camera
 			v = cross(w, u);
 
 			/* width and height have this ratio to spot easily possible transpose issues */
-			lower_left_corner = origin - half_width * u - half_height * v - w;
-			horizontal = 2.0 * half_width * u;
-			vertical = 2.0 * half_height * v;
+			lower_left_corner = origin - half_width * focus_dist * u - half_height * focus_dist * v - focus_dist * w;
+			horizontal = 2.0 * half_width * focus_dist * u;
+			vertical = 2.0 * half_height * focus_dist * v;
 		}
 
-		ray get_ray(float u, float v)
+		ray get_ray(float s, float t)
 		{
-			return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			vec3 rd = lens_radius * random_in_unit_disk();
+			vec3 offset = u * rd.x() + v * rd.y();
+
+			return ray(origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset);
 		}
 
 		vec3 origin;
 		vec3 lower_left_corner;
 		vec3 horizontal;
 		vec3 vertical;
+		vec3 u, v, w;
+		float lens_radius;
 };
 
 #endif /* CAMERAHPP */
